645470e1e8d20b15ae9c10ff4f13ed474f2bb398
lvris <lvris@qq.com>
2025-11-25 06:45:26 +0200

    🧹No extra folder

--- components/HomePage/ServiceSection.tsx
+++ components/HomePage/ServiceSection.tsx
@@ -1,4 +1,25 @@
-import { services } from "@/mocks/Services/data";
+import { HiMiniTag, HiMusicalNote, HiRocketLaunch } from "react-icons/hi2";
+
+const services = [
+  {
+    id: 1,
+    icon: <HiMiniTag />,
+    title: "discounts",
+    text: "discounted products",
+  },
+  {
+    id: 2,
+    icon: <HiMusicalNote />,
+    title: "music",
+    text: "musical instruments",
+  },
+  {
+    id: 3,
+    icon: <HiRocketLaunch />,
+    title: "toys",
+    text: "toys",
+  },
+];
 
 const ServiceSection = () => {
   return (
